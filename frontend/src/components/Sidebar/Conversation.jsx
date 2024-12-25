d8bac569877728503324b186c0a62c1700f7522f
Himanshi-jain25 <himanshijain2501@gmail.com>
2024-12-25 17:26:05 +0530

    secondpartcompleted

---
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Conversation = () => {
+    return (
+        <div>
+            <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+                <div className='avatar online'>
+                    <div className='w-12 rounded-full'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+
+            </div>
+            <div>
+           <div className='flex flex-col flex-1'>
+            <div className='flex gap-3 justify-between'>
+                <p className='font-bold text-gray-200'> Himanshi</p>
+                <span className='text-xl'>👩🏼</span>
+            </div>
+           </div>
+            </div>
+            <div className='divider my-0 py-0 h-1' />
+
+
+        </div>
+
+    )
+}
+
+export default Conversation
+
+
+
+/*import React from 'react'
+
+const Conversation = () => {
+    return (
+        <div>
+            <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+                <div className='avatar online'>
+                    <div className='w-12 rounded-full'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+
+            </div>
+            <div>
+           <div className='flex flex-col flex-1'>
+            <div className='flex gap-3 justify-between'>
+                <p className='font-bold text-gray-200'> Himanshi</p>
+                <span className='text-xl'>👩🏼</span>
+            </div>
+           </div>
+            </div>
+            <div className='divider my-0 py-0 h-1' />
+
+
+        </div>
+
+    )
+}
+
+export default Conversation
+*/
