51b3f32a17124cc0ca88c05c545ed7267f083d19
Himanshijain-2005 <himanshijain2501@gmail.com>
2025-07-15 00:23:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy:{
       "/api":
       {
-        target: "http://localhost:4000",
+        target: "https://whisperwave-kvja.onrender.com",
       }
 
     },
