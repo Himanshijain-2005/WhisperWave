5b8ad2b976ab444ccfc170c24173c87c1d1d5c41
Himanshijain-2005 <himanshijain2501@gmail.com>
2025-07-15 11:10:26 +0530

    Update useLogout.js

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -8,7 +8,7 @@ const useLogout = () => {
    const logout=async()=>{
    setLoading(true);
    try {
-        const res=await fetch("/api/auth/logout",{
+        const res=await fetch("https://whisperwave-kvja.onrender.com/api/auth/logout",{
           method:"POST",
           headers:{"Content-Type" :"application/json"},
         })
